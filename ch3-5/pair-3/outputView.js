ef3fc0ec89dadd67554847ca148e550fb68b6d84
박건규 <geongyu09@gmail.com>
2026-03-12 14:46:20 +0900

    refactor(outputView): room 도메인 예외 관련 출력 관심사 분리
    Co-authored-by: Yean <kjan0929@naver.com>

--- ch3-5/pair-3/outputView.js
+++ ch3-5/pair-3/outputView.js
@@ -5,3 +5,11 @@ export function printRegisterResult(name) {
 export function printLoginRequiredMessage() {
   console.log("로그인이 필요합니다.");
 }
+
+export function printInvalidRoom(roomId) {
+  console.log("존재하지 않는 룸입니다: " + roomId);
+}
+
+export function printCapacityExcess(capacity) {
+  console.log("인원이 초과되었습니다. 최대 수용 인원: " + capacity + "명");
+}
